026be739e08828907bfc3e7877f828be21bab46f
andrewksloan <andrewksloan3@gmail.com>
2022-09-18 13:37:43 -0700

    Add backend basic server code, additional backend dependencies.

--- server/app.js
+++ server/app.js
@@ -11,12 +11,8 @@ app.use(cors())
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
-// app.use('/posts', postRoutes);
-// app.use('/user', userRoutes);
-
 const PORT = process.env.PORT || 5000;
 
-// Clarify
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
      .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
      .catch((err) => console.log(err));
